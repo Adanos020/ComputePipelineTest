9b73c232ff7c56809fb498280daaa8ebd0a5c5b7
Adanos020 <adanos020@gmail.com>
2025-01-26 00:43:48 +0000

    Remove unnecessary constructors

--- ComputePipeline/Include/Actions/DecodeImage.hpp
+++ ComputePipeline/Include/Actions/DecodeImage.hpp
@@ -4,6 +4,5 @@
 
 class ActionDecodeImage : public Action {
 public:
-    ActionDecodeImage() = default;
     virtual std::optional<std::shared_ptr<ActionOutput>> execute(const ActionOutput* previous_output) override;
 };
--- ComputePipeline/Include/Actions/DecompressData.hpp
+++ ComputePipeline/Include/Actions/DecompressData.hpp
@@ -4,6 +4,5 @@
 
 class ActionDecompressData : public Action {
 public:
-    ActionDecompressData() = default;
     virtual std::optional<std::shared_ptr<ActionOutput>> execute(const ActionOutput* previous_output) override;
 };
